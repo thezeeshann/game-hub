9559329809ea3714b852a5f4fd5c426fd0be4b8e
thezeeshann <info.zeeshann@gmail.com>
2024-02-21 23:23:18 +0530

    feat: Added spinner component

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Spinner.Module.css"
+
+const Spinner: React.FC = () => {
+  return <div className="flex items-center justify-center h-screen"><span className="spinner"></span></div>;
+};
+
+export default Spinner;
