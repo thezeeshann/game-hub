ce1c5a866227df6ea3608c7342331a99a0b9c409
thezeeshann <info.zeeshann@gmail.com>
2024-02-24 01:09:56 +0530

    feat: Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import {Routes,Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Games from "./pages/Games";
 import Navbar from "./components/Navbar";
+import SingleGame from "./pages/SingleGame";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Navbar/>
-     <Routes>
-      <Route path="/" element={<Home/>} />
-     </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Games />} />
+          <Route path="/games/:gameId" element={<SingleGame />} />
+        </Route>
+      </Routes>
     </>
   );
 }
