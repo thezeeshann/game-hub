add76a16d332a0f3150373f6b28fd41d333a6ad7
thezeeshann <info.zeeshann@gmail.com>
2024-02-26 22:22:33 +0530

    feat: Added error component

---
+++ src/components/Error.tsx
@@ -0,0 +1,14 @@
+import {Link} from "react-router-dom"
+
+const Error = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-y-3">
+        <p className="text-xl font-semibold text-red-300">Page not found!</p>
+         <Link to="/" >
+         <span className="text-sm underline cursor-pointer">go back to home</span>
+         </Link>
+    </div>
+  )
+}
+
+export default Error
