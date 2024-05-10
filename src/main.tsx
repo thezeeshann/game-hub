93d1b1edca53193bb44ee05a1a641ef063340844
thezeeshann <info.zeeshann@gmail.com>
2024-05-10 12:23:44 +0530

    refactor: update components

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import GameContextProvider from "./context/GameContext.tsx";
+import SearchContextProvider from "./context/SearchContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <GameContextProvider>
-        <App />
-      </GameContextProvider>
+      <SearchContextProvider>
+        <GameContextProvider>
+          <App />
+        </GameContextProvider>
+      </SearchContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,15 +1,21 @@
 import ErrorMessage from "../components/ErrorMessage";
 import GameCard from "../components/GameCard";
 import Spinner from "../components/Spinner";
-import { useGamesContext } from "../lib/hooks";
+import { useGamesContext, useSearchContext } from "../lib/hooks";
+import Heading from "../components/H1";
 
 const Games = () => {
   const { games, isLoading, errorMessage } = useGamesContext();
+  const { searchQuery } = useSearchContext();
+
+  const filterGames = games.filter((game) =>
+    game.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
     <section className="flex flex-col font-Poppins gap-y-5">
       <div className="flex flex-col gap-y-2">
-        <p className="text-6xl font-bold">New and trending</p>
+        <Heading>New and trending</Heading>
         <span className="">Based on player counts and release date</span>
       </div>
 
@@ -21,7 +27,7 @@ const Games = () => {
             <ErrorMessage message={errorMessage} />
           ) : (
             <div className="grid grid-cols-3 gap-x-5 ">
-              {games.map((game) => (
+              {filterGames.map((game) => (
                 <GameCard key={game.id} game={game} />
               ))}
             </div>
