136dd0184614219e971c1191b1b455665aa124a1
thezeeshann <info.zeeshann@gmail.com>
2024-05-10 12:20:29 +0530

    feat: Added seach context

---
+++ src/context/SearchContext.tsx
@@ -0,0 +1,33 @@
+import { useState, createContext } from "react";
+
+type SearchContextProviderProps = {
+  children: React.ReactNode;
+};
+
+type TSeachContext = {
+  searchQuery: string;
+  handleChangeSearchQuery: (newValue: string) => void;
+};
+
+export const SearchContext = createContext<TSeachContext | null>(null);
+
+const SearchContextProvider = ({ children }: SearchContextProviderProps) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleChangeSearchQuery = (newValue: string) => {
+    setSearchQuery(newValue);
+  };
+
+  return (
+    <SearchContext.Provider
+      value={{
+        searchQuery,
+        handleChangeSearchQuery,
+      }}
+    >
+      {children}
+    </SearchContext.Provider>
+  );
+};
+
+export default SearchContextProvider;
